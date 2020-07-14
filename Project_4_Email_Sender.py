3b9ffec302d787b3eeabce786524fb1d297a842c
Sagar Patel <52515373+sagarbpatel31@users.noreply.github.com>
2020-07-14 15:04:21 +0530

    Update Project_4_Email_Sender.py

--- Project_4_Email_Sender.py
+++ Project_4_Email_Sender.py
@@ -7,8 +7,8 @@ def SendEmail(to,content):
     server=st.SMTP("smtp.gmail.com",587)
     server.ehlo()
     server.starttls()
-    server.login('sagarp3199@gmail.com','399ansimla2603')
-    server.sendmail('sagarp3199@gmail.com',to,content)
+    server.login('email id','passwd')
+    server.sendmail('email id',to,content)
     server.close()
 
 SendEmail(to,content)
